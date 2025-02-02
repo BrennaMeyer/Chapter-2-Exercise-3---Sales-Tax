df743b30aa8d4d8c9d38dba67cf7400b88405b44
Brenna Meyer <brennameyer9@gmail.com>
2025-02-02 14:01:21 -0500

    Exercise 3 Part 2 revised program

--- Chapter 2 Exercise 3 - Sales Tax.cpp
+++ Chapter 2 Exercise 3 - Sales Tax.cpp
@@ -2,19 +2,27 @@
 //
 
 #include <iostream>
+#include <iomanip> 
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int purchase = 95;
+    float sateSalesTax = .04,
+        countySalesTax = .02;
+    //set decimal percision to 2 places after the decimal point. Ex 0.00
+    cout << setprecision(2) << fixed << endl;
+
+    cout << "The total state sales tax on $95 dollar purchase is $";
+    cout << purchase * sateSalesTax << endl;
+
+    cout << "The total county sales tax on $95 dollar purchase is $"; 
+    cout << purchase * countySalesTax << endl << endl; 
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
